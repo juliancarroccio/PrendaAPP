3a4e19dd65c712cebf484ef375903193eb457683
unknown <you@example.com>
2021-01-06 21:12:55 -0300

    Se Agrega archivo y se resguarda variables de entorno

--- APICRUD/src/app.py
+++ APICRUD/src/app.py
@@ -3,10 +3,21 @@ from sqlalchemy import ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from sqlalchemy.orm import relationship
+from dotenv import load_dotenv
+import os
 
+#Load enviorment variables
+load_dotenv()
+DB_USER = os.getenv("DATABASE_USER")
+DB_PASS = os.getenv("DATABASE_PASSWORD")
+DB_ADDRESS = os.getenv("DATABASE_ADDRESS")
+DB_NAME = os.getenv("DATABASE_NAME")
+API_HOST = os.getenv("API_HOST")
+API_PORT = os.getenv("API_PORT")
 
+#Open DB to modeling
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/dbappscanner'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(DB_USER, DB_PASS, DB_ADDRESS, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -132,7 +143,7 @@ class ProductoSchema(ma.Schema):
 producto_schema = ProductoSchema()
 productos_schema = ProductoSchema(many=True)
 
-
+#Create a prenda and persist in database
 @app.route('/prenda', methods=['POST'])
 def create_prenda():
     id_marca = request.json['id_marca']
@@ -148,27 +159,27 @@ def create_prenda():
     id_familia = request.json['id_familia']
 
 
-    new_task = Producto(codigoBarra, descripcion_producto, id_color, id_familia, id_industria, id_marca, id_proveedor, id_talle, iva, precio, stock)
+    new_prenda = Producto(codigoBarra, descripcion_producto, id_color, id_familia, id_industria, id_marca, id_proveedor, id_talle, iva, precio, stock)
 
-    db.session.add(new_task)
+    db.session.add(new_prenda)
     db.session.commit()
 
-    return producto_schema.jsonify(new_task)
+    return producto_schema.jsonify(new_prenda)
 
-
+#Get all prendas in the database
 @app.route('/prenda', methods=['GET'])
 def get_prendas():
-    all_tasks = Producto.query.all()
-    result = productos_schema.dump(all_tasks)
+    all_prendas = Producto.query.all()
+    result = productos_schema.dump(all_prendas)
     return jsonify(result)
 
-
+#Get a prenda according to its codebar
 @app.route('/prenda/<codigoBarra>', methods=['GET'])
 def get_prenda(codigoBarra):
     producto = Producto.query.get(codigoBarra)
     return producto_schema.jsonify(producto)
 
-
+#Edit a prenda according to its codebar
 @app.route('/prenda/<codigoBarra>', methods=['PUT'])
 def update_prenda(codigoBarra):
     prenda = Producto.query.get(codigoBarra)
@@ -201,7 +212,7 @@ def update_prenda(codigoBarra):
 
     return producto_schema.jsonify(prenda)
 
-
+#Delete a prenda according to its codebar
 @app.route('/prenda/<codigoBarra>', methods=['DELETE'])
 def delete_prenda(codigoBarra):
     prenda = Producto.query.get(codigoBarra)
@@ -209,6 +220,6 @@ def delete_prenda(codigoBarra):
     db.session.commit()
     return producto_schema.jsonify(prenda)
 
-
+#init API
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=API_HOST, port=API_PORT, debug=True)
